4e9a858df050714a9e3e383b00deef2b0a3e8153
Andy Gross <andogro@gmail.com>
2016-01-26 14:59:26 -0700

    added js file

---
+++ src/js/home.js
@@ -0,0 +1,25 @@
+$(document).on('ready', function() {
+  console.log('sanity check!');
+
+$('.homepage-image').on('hover', function() {
+
+});
+
+
+// Validate Email
+
+
+
+
+
+
+// Slider
+
+
+
+
+});
+
+
+
+
