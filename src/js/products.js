46b540bc50b71bb7cbf939b65498b9db5b590335
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-01-27 16:45:28 -0700

    added filter functionality to products page

--- src/js/products.js
+++ src/js/products.js
@@ -19,14 +19,59 @@ $(document).on('ready', function() {
 
 
 
+  //write a get all
+  //add a return all button to the dom
+  // event.stopPropagation();
+  $('.waitForIt').hide();
 
+  $($getBlue).on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').show();
+    $($blue).show();
+    $($red).hide();
+    $($gray).hide();
 
+  });
 
+  $($getGray).on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').show();
+    $($gray).show();
+    $($red).hide();
+    $($blue).hide();
+  });
 
+  $($getRed).on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').show();
+    $($red).show();
+    $($blue).hide();
+    $($gray).hide();
+  });
 
+  $($getRad).on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').show();
+    $($rad).show();
+    $($g19).hide();
+  });
 
+  $($getG19).on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').show();
+    $($g19).show();
+    $($rad).hide();
+  });
 
-
+  $('.waitForIt').on('click', function () {
+    event.stopPropagation();
+    $('.waitForIt').hide();
+    $($rad).show();
+    $($g19).show();
+    $($blue).show();
+    $($gray).show();
+    $($red).show();
+  });
 
 });
 
