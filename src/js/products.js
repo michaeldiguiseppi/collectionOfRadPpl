a6b40ee83440d9c8ebba36c19cd2cfb81fb6ba26
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-01-27 14:42:54 -0700

    add class to html, create js file

---
+++ src/js/products.js
@@ -0,0 +1,33 @@
+// add scripts
+
+$(document).on('ready', function() {
+  console.log('sanity check!');
+
+  var $red = $('.red');
+  var $gray = $('.gray');
+  var $blue = $('.blue');
+
+  var $g19 = $('.g19');
+  var $rad = $('.rad');
+
+  var $getBlue = $('.getBlue');
+  var $getRed = $('.getRed');
+  var $getGray = $('.getGray');
+
+  var $getRad = $('.getRad');
+  var $getG19 = $('.getG19');
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
