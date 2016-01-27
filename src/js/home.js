b6535da2637d406c8db4376c86f1eb8fd269bd08
Andy Gross <andogro@gmail.com>
2016-01-26 17:15:35 -0700

    square images, glyph a href

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,14 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 
-$('.homepage-image').on('hover', function() {
+$('.homepage-image').hover(function() {
+   $(this).fadeTo(1000, 0.4);
+ }, function() {
+   $(this).fadeTo(1000, 1);
+ });
 
-});
 
+// $(html).hide().appendTo("#mycontent").fadeIn(1000);
 
 // Validate Email
 
